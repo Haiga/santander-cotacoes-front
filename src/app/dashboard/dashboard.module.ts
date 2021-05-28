79db2b584414ffac0e8f7978364397146531643b
Pedro <pedro_rga@hotmail.com>
2021-05-28 13:58:48 -0300

    locale, and css variation using data

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,8 +4,6 @@ import { CommonModule } from '@angular/common';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { StockCardComponent } from './stock-card/stock-card.component';
 
-
-
 @NgModule({
   declarations: [
     DashboardPageComponent,
